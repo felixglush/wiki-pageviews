5f6bcc92800f187697824f8a2ad1b7467e4031f9
Felix Glushchenkov <felixglush@gmail.com>
2022-04-10 14:51:19 -0400

    Refactor queries to own module

--- db/config.py
+++ db/config.py
@@ -1,7 +1,7 @@
 import psycopg2
 from psycopg2 import sql
 from configparser import ConfigParser
-import schema_constants
+import sql_queries
 
 
 def read_db_ini(filename='db/database.ini', section='postgresql'):
@@ -26,65 +26,16 @@ def read_db_ini(filename='db/database.ini', section='postgresql'):
 def populate_tables_with_defaults():
     """Populates agents, access methods, and granularity tables with default values."""
 
-    insert_into_agents = \
-        """
-        INSERT INTO {table} ({column})
-        VALUES
-            ('{all_agents}'),
-            ('{user}'),
-            ('{automated}'),
-            ('{spider}')
-        ON CONFLICT ({column}) DO NOTHING;
-        """.format(
-            table=schema_constants.AGENTS_TABLE,
-            column=schema_constants.AGENTS_NAME_COLUMN,
-            all_agents='all-agents',
-            user='user',
-            automated='automated',
-            spider='spider'
-        )
-
-    insert_into_access_table = \
-        """
-        INSERT INTO {table} ({column})
-        VALUES
-            ('{all_access}'),
-            ('{mobile_app}'),
-            ('{mobile_web}'),
-            ('{desktop}')
-        ON CONFLICT ({column}) DO NOTHING;
-        """.format(
-            table=schema_constants.ACCESS_TABLE,
-            column=schema_constants.ACCESS_NAME_COLUMN,
-            all_access='all-access',
-            mobile_app='mobile-app',
-            mobile_web='mobile-web',
-            desktop='desktop'
-        )
-
-    insert_into_granularity_table = \
-        """
-        INSERT INTO {table} ({column})
-        VALUES
-            ('{daily}'),
-            ('{monthly}')
-        ON CONFLICT ({column}) DO NOTHING;
-        """.format(
-            table=schema_constants.GRANULARITY_TABLE,
-            column=schema_constants.GRANULARITY_NAME_COLUMN,
-            daily='daily',
-            monthly='monthly'
-        )
-
     conn = None
     try:
         params = read_db_ini()
         conn = psycopg2.connect(**params)
         conn.autocommit = True
         cursor = conn.cursor()
-        cursor.execute(insert_into_access_table)
-        cursor.execute(insert_into_agents)
-        cursor.execute(insert_into_granularity_table)
+
+        cursor.execute(sql_queries.insert_into_access_table)
+        cursor.execute(sql_queries.insert_into_agents_table)
+        cursor.execute(sql_queries.insert_into_granularity_table)
 
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -96,64 +47,6 @@ def populate_tables_with_defaults():
 
 def create_tables():
     """Creates the tables in the database created by `created_db()`."""
-    articles_table = \
-        """
-        CREATE TABLE IF NOT EXISTS {0} (
-            id SERIAL PRIMARY KEY,
-            name TEXT NOT NULL UNIQUE
-        );
-        """.format(
-            schema_constants.ARTICLES_TABLES
-        )
-
-    agents_table = \
-        """
-        CREATE TABLE IF NOT EXISTS {0} (
-            id SERIAL PRIMARY KEY,
-            name TEXT NOT NULL UNIQUE
-        );
-        """.format(
-            schema_constants.AGENTS_TABLE
-        )
-
-    access_table = \
-        """
-        CREATE TABLE IF NOT EXISTS {0} (
-            id SERIAL PRIMARY KEY,
-            name TEXT NOT NULL UNIQUE
-        );
-        """.format(
-            schema_constants.ACCESS_TABLE
-        )
-
-    granularity_table = \
-        """
-        CREATE TABLE IF NOT EXISTS {0} (
-            id SERIAL PRIMARY KEY,
-            name TEXT NOT NULL UNIQUE
-        );
-        """.format(
-            schema_constants.GRANULARITY_TABLE
-        )
-
-    page_views_table = \
-        """
-        CREATE TABLE IF NOT EXISTS {0} (
-            id SERIAL PRIMARY KEY,
-            article_id INTEGER REFERENCES {1} (id),
-            access_id INTEGER REFERENCES {2} (id),
-            agent_id INTEGER REFERENCES {3} (id),
-            granularity_id INTEGER REFERENCES {4} (id),
-            pageviews INT,
-            date DATE
-        );
-        """.format(
-            schema_constants.PAGE_VIEWS_TABLE,
-            schema_constants.ARTICLES_TABLES,
-            schema_constants.ACCESS_TABLE,
-            schema_constants.AGENTS_TABLE,
-            schema_constants.GRANULARITY_TABLE
-        )
 
     conn = None
     try:
@@ -162,11 +55,11 @@ def create_tables():
         conn.autocommit = True
         cursor = conn.cursor()
 
-        cursor.execute(articles_table)
-        cursor.execute(access_table)
-        cursor.execute(granularity_table)
-        cursor.execute(agents_table)
-        cursor.execute(page_views_table)
+        cursor.execute(sql_queries.create_articles_table)
+        cursor.execute(sql_queries.create_access_table)
+        cursor.execute(sql_queries.create_granularity_table)
+        cursor.execute(sql_queries.create_agents_table)
+        cursor.execute(sql_queries.create_page_views_table)
 
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -196,15 +89,12 @@ def create_db():
         cursor = conn.cursor()
 
         # Preparing query to create a database
-        cursor.execute(
-            "SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{0}';".format(
-                database_name)
-        )
+        cursor.execute(sql_queries.does_db_exist(database_name))
         exists = cursor.fetchone()
         # Create the database
         if not exists:
             print(f'Database does not exist. Creating {database_name}...')
-            cursor.execute('CREATE DATABASE {0} WITH OWNER = {1}'.format(
+            cursor.execute(sql_queries.create_db(
                 database_name, params['user']))
         else:
             print(f'Database {database_name} already exists.')
@@ -229,30 +119,10 @@ def create_user():
             host='127.0.0.1',
             port='5432',
         )
-
+        conn.autocommit = True
         cur = conn.cursor()
-
-        create_role_if_not_exists = """
-                                    DO
-                                    $do$
-                                    BEGIN
-                                        IF NOT EXISTS (
-                                            SELECT FROM pg_catalog.pg_roles
-                                            WHERE  rolname = '{0}') THEN
-                                            CREATE ROLE {0} LOGIN PASSWORD {1};
-                                            ALTER ROLE {0} WITH CREATEDB;
-                                            GRANT {0} TO postgres;
-                                    END IF;
-                                    END
-                                    $do$;"""
-
-        query = sql.SQL(create_role_if_not_exists).format(
-            sql.Identifier(params['user']),
-            sql.Literal(params['password']),
-        )
-        cur.execute(query.as_string(conn))
-
-        cur.execute("COMMIT")
+        cur.execute(sql_queries.create_role_if_not_exists(
+            params['user'], params['password']))
         cur.close()
     except (Exception, psycopg2.DatabaseError) as e:
         print('User already exists.')
@@ -261,8 +131,12 @@ def create_user():
             conn.close()
 
 
-if __name__ == '__main__':
+def config():
     create_user()
     create_db()
     create_tables()
     populate_tables_with_defaults()
+
+
+if __name__ == '__main__':
+    config()
---
+++ db/sql_queries.py
@@ -0,0 +1,136 @@
+import schema_constants
+
+insert_into_agents_table = \
+    """
+    INSERT INTO {table} ({column})
+    VALUES
+        ('{all_agents}'),
+        ('{user}'),
+        ('{automated}'),
+        ('{spider}')
+    ON CONFLICT ({column}) DO NOTHING;
+    """.format(
+        table=schema_constants.AGENTS_TABLE,
+        column=schema_constants.AGENTS_NAME_COLUMN,
+        all_agents='all-agents',
+        user='user',
+        automated='automated',
+        spider='spider'
+    )
+
+insert_into_access_table = \
+    """
+    INSERT INTO {table} ({column})
+    VALUES
+        ('{all_access}'),
+        ('{mobile_app}'),
+        ('{mobile_web}'),
+        ('{desktop}')
+    ON CONFLICT ({column}) DO NOTHING;
+    """.format(
+        table=schema_constants.ACCESS_TABLE,
+        column=schema_constants.ACCESS_NAME_COLUMN,
+        all_access='all-access',
+        mobile_app='mobile-app',
+        mobile_web='mobile-web',
+        desktop='desktop'
+    )
+
+insert_into_granularity_table = \
+    """
+    INSERT INTO {table} ({column})
+    VALUES
+        ('{daily}'),
+        ('{monthly}')
+    ON CONFLICT ({column}) DO NOTHING;
+    """.format(
+        table=schema_constants.GRANULARITY_TABLE,
+        column=schema_constants.GRANULARITY_NAME_COLUMN,
+        daily='daily',
+        monthly='monthly'
+    )
+
+
+create_articles_table = \
+    """
+    CREATE TABLE IF NOT EXISTS {0} (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE
+    );
+    """.format(
+        schema_constants.ARTICLES_TABLES
+    )
+
+create_agents_table = \
+    """
+    CREATE TABLE IF NOT EXISTS {0} (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE
+    );
+    """.format(
+        schema_constants.AGENTS_TABLE
+    )
+
+create_access_table = \
+    """
+    CREATE TABLE IF NOT EXISTS {0} (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE
+    );
+    """.format(
+        schema_constants.ACCESS_TABLE
+    )
+
+create_granularity_table = \
+    """
+    CREATE TABLE IF NOT EXISTS {0} (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE
+    );
+    """.format(
+        schema_constants.GRANULARITY_TABLE
+    )
+
+create_page_views_table = \
+    """
+    CREATE TABLE IF NOT EXISTS {0} (
+        id SERIAL PRIMARY KEY,
+        article_id INTEGER REFERENCES {1} (id),
+        access_id INTEGER REFERENCES {2} (id),
+        agent_id INTEGER REFERENCES {3} (id),
+        granularity_id INTEGER REFERENCES {4} (id),
+        pageviews INT,
+        date DATE
+    );
+    """.format(
+        schema_constants.PAGE_VIEWS_TABLE,
+        schema_constants.ARTICLES_TABLES,
+        schema_constants.ACCESS_TABLE,
+        schema_constants.AGENTS_TABLE,
+        schema_constants.GRANULARITY_TABLE
+    )
+
+
+def does_db_exist(database):
+    return "SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{0}';".format(database)
+
+
+def create_db(database, user):
+    return 'CREATE DATABASE {0} WITH OWNER = {1}'.format(database, user)
+
+
+def create_role_if_not_exists(rolename, password):
+    return \
+        """
+        DO
+        $do$
+        BEGIN
+            IF NOT EXISTS (
+                SELECT FROM pg_catalog.pg_roles
+                WHERE  rolname = '{0}') THEN
+                CREATE ROLE {0} LOGIN PASSWORD {1};
+                ALTER ROLE {0} WITH CREATEDB;
+                GRANT {0} TO postgres;
+        END IF;
+        END
+        $do$;""".format(rolename, password)
